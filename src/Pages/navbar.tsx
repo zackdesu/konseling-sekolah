cfdb5fae252a137ca71228cc226c1625fe8ce6f4
zackdesu <zarchxxx@gmail.com>
2023-09-04 12:42:48 +0700

    h3 add link

--- src/Pages/navbar.tsx
+++ src/Pages/navbar.tsx
@@ -3,7 +3,9 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <div className="flex items-center justify-between px-20 py-5 bg-cyan-500 text-zinc-100 fixed z-50 w-full">
-      <h3>{import.meta.env.VITE_PRODUCT_NAME}</h3>
+      <h3>
+        <Link to={"/"}>{import.meta.env.VITE_PRODUCT_NAME}</Link>
+      </h3>
       <div className="w-1/4 flex justify-between mr-20">
         <Link to={"/"}>Halaman Utama</Link>
         <Link to={"/profil"}>Profil</Link>
