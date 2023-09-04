d5edcfc347ce34e13032647de5a665502f12cc1c
zackdesu <zarchxxx@gmail.com>
2023-09-04 12:28:53 +0700

    welcome home design

--- src/home.tsx
+++ src/home.tsx
@@ -1,7 +1,8 @@
 const Home = () => {
   return (
     <div className="flex flex-col justify-center items-center h-full w-full">
-      <h1>Selamat datang di {import.meta.env.VITE_PRODUCT_NAME}</h1>
+      <p className="text-lg">Selamat datang di</p>
+      <h1 className="mb-3 -mt-1">{import.meta.env.VITE_PRODUCT_NAME}!</h1>
       <div className="">
         <button className="text-white px-4 py-2 mx-2 my-2 rounded-xl bg-cyan-500">
           Buat Akun
