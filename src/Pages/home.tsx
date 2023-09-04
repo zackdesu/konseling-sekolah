e5c467002785f0729c4132a4342d533bcf9032fc
zackdesu <zarchxxx@gmail.com>
2023-09-04 13:59:29 +0700

    optimize class from css

--- src/Pages/home.tsx
+++ src/Pages/home.tsx
@@ -4,12 +4,8 @@ const Home = () => {
       <p className="text-lg">Selamat datang di</p>
       <h1 className="mb-3 -mt-1">{import.meta.env.VITE_PRODUCT_NAME}!</h1>
       <div className="">
-        <button className="text-white px-4 py-2 mx-2 my-2 rounded-xl bg-cyan-500">
-          Buat Akun
-        </button>
-        <button className="px-4 py-2 mx-2 my-2 rounded-xl border-2 border-cyan-500">
-          Masuk Akun
-        </button>
+        <button className="normalbutton">Buat Akun</button>
+        <button className="invertedbutton">Masuk Akun</button>
       </div>
     </div>
   );
