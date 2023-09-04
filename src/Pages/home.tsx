aa5557bf654000b1d21ed969465309b28ca3e2c3
zackdesu <zarchxxx@gmail.com>
2023-09-04 14:05:16 +0700

    add button href

--- src/Pages/home.tsx
+++ src/Pages/home.tsx
@@ -1,11 +1,17 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
   return (
     <div className="flex flex-col justify-center items-center h-full w-full">
       <p className="text-lg">Selamat datang di</p>
       <h1 className="mb-3 -mt-1">{import.meta.env.VITE_PRODUCT_NAME}!</h1>
       <div className="">
-        <button className="normalbutton">Buat Akun</button>
-        <button className="invertedbutton">Masuk Akun</button>
+        <Link to={"/login"}>
+          <button className="normalbutton">Buat Akun</button>
+        </Link>
+        <Link to={"/register"}>
+          <button className="invertedbutton">Masuk Akun</button>
+        </Link>
       </div>
     </div>
   );
