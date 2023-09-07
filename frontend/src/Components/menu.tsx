671296e68dafd4678f162502c99199cc138c6b03
zackdesu <zarchxxx@gmail.com>
2023-09-07 14:02:06 +0700

    del: notification route menu

--- frontend/src/Components/menu.tsx
+++ frontend/src/Components/menu.tsx
@@ -1,18 +1,15 @@
-import { AiFillCompass, AiFillHome, AiFillBell } from "react-icons/ai";
+import { AiFillCompass, AiFillHome } from "react-icons/ai";
 import { BsFillPersonFill } from "react-icons/bs";
 import { Link } from "react-router-dom";
 const Menu = () => {
   return (
     <div className="mx-auto fixed bottom-5 left-0 right-0 max-w-[250px] w-[55%] min-[500px]:w-[40%] md:w-1/3">
-      <div className="bg-zinc-200 px-4 py-3 flex justify-between items-center rounded-full relative z-30">
+      <div className="bg-zinc-200 px-8 py-3 flex justify-between items-center rounded-full relative z-30">
         <Link to={"/"}>
           <AiFillHome size={20} className="min-w-[20px]" />
         </Link>
         <Link to={"/feed"}>
           <AiFillCompass size={20} className="max-[450px]:min-w-[20px]" />
-        </Link>
-        <Link to={"/notification"}>
-          <AiFillBell size={20} className="max-[450px]:min-w-[20px]" />
         </Link>
         <Link to={"/profil"}>
           <BsFillPersonFill size={20} className="max-[450px]:min-w-[20px]" />
