fae76a7d272b20836802030d5b946d5db2a57648
zackdesu <zarchxxx@gmail.com>
2023-09-07 14:15:22 +0700

    feat: image randomize

--- frontend/src/Pages/home.tsx
+++ frontend/src/Pages/home.tsx
@@ -1,8 +1,16 @@
 import { Link } from "react-router-dom";
 
 const Home = () => {
+  const randomize = [
+    "/random/1.svg",
+    "/random/2.svg",
+    "/random/3.svg",
+    "/random/4.svg",
+  ];
+  const randomMath = Math.round(Math.random() * 3);
   return (
     <div className="flex flex-col justify-center items-center h-full w-full">
+      <img src={randomize[randomMath]} width={296} height={296} />
       <p className="text-lg">Selamat datang di</p>
       <h1 className="mb-3 -mt-1">{import.meta.env.VITE_PRODUCT_NAME}!</h1>
       <div className="">
