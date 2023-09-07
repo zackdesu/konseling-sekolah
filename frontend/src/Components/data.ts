15f97c123d1b8c016bb7e53bb61e6fa7429b5cc7
zackdesu <zarchxxx@gmail.com>
2023-09-07 10:43:53 +0700

    feat: data

---
+++ frontend/src/Components/data.ts
@@ -0,0 +1,42 @@
+export const dataPost: DataPost[] = [
+  {
+    id: "1",
+    username: "zackdesu",
+    private: true,
+    anonym: true,
+    likes: 123,
+    post: "Jika aku adalah sebuah burung, mungkin aku akan terbebas dari segala pengekangan yang saya alami.",
+  },
+  {
+    id: "2",
+    username: "zackdesu",
+    private: true,
+    anonym: true,
+    likes: 123,
+    post: "Jika aku adalah sebuah burung, mungkin aku akan terbebas dari segala pengekangan yang saya alami.",
+  },
+  {
+    id: "3",
+    username: "jofanctan",
+    private: false,
+    anonym: false,
+    likes: 123,
+    post: "Banyaknya tuntutan dan masalah-masalah belum terselesaikan.",
+  },
+  {
+    id: "4",
+    username: "kielll97",
+    private: false,
+    anonym: false,
+    likes: 123,
+    post: "Demam panggung...",
+  },
+  {
+    id: "5",
+    username: "zackdesu",
+    private: false,
+    anonym: false,
+    likes: 123,
+    post: "Jika aku adalah sebuah burung, mungkin aku akan terbebas dari segala pengekangan yang saya alami.",
+  },
+];
