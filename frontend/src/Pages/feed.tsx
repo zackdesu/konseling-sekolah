da9df17cc070eb132683b068c3897f289f2ad0e1
zackdesu <zarchxxx@gmail.com>
2023-09-07 13:25:59 +0700

    feat: button create feed

--- frontend/src/Pages/feed.tsx
+++ frontend/src/Pages/feed.tsx
@@ -3,13 +3,16 @@ import FeedPost from "../Components/feedpost";
 
 const Feed = () => {
   return (
-    <div className="py-20 flex flex-wrap justify-center items-center mx-5">
-      {dataPost
-        .filter((e) => !e.private)
-        .map((e, i) => (
-          <FeedPost data={e} key={i} />
-        ))}
-    </div>
+    <>
+      <div className="py-20 flex flex-wrap items-center justify-center mx-5">
+        <button className="normalbutton ml-auto mr-12">+ Buat Feed</button>
+        {dataPost
+          .filter((e) => !e.private)
+          .map((e, i) => (
+            <FeedPost data={e} key={i} />
+          ))}
+      </div>
+    </>
   );
 };
 
