175e217e0b39421ad2576b7609b8d29818f94ca7
zackdesu <zarchxxx@gmail.com>
2023-09-07 07:42:02 +0700

    feat: about page

---
+++ frontend/src/Pages/tentang.tsx
@@ -0,0 +1,10 @@
+const Tentang = () => {
+  return (
+    <div className="pt-20 flex h-full w-full flex-col items-center justify-center">
+      <h1>Tentang Kami</h1>
+      <p>Project ini dibuat untuk lomba KTI</p>
+    </div>
+  );
+};
+
+export default Tentang;
