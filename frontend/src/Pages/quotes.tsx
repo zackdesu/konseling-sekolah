3ca337f127f6eef8870af3f525bd149331f92ba3
zackdesu <zarchxxx@gmail.com>
2023-09-08 08:16:53 +0700

    feat: quotes page

---
+++ frontend/src/Pages/quotes.tsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+import QuotePost from "../Components/quotepost";
+import { QuotesData } from "../Components/data";
+
+const Quotes = () => {
+  return (
+    <div className="py-20 flex flex-wrap items-center justify-center mx-5">
+      <Link to={"/createfeed"} className="ml-auto normalbutton mr-12">
+        + Buat Feed
+      </Link>
+      {QuotesData.map((e, i) => (
+        <QuotePost data={e} key={i} />
+      ))}
+    </div>
+  );
+};
+
+export default Quotes;
