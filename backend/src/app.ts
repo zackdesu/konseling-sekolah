06b79e842072bc2ebf1cd46acf1d8ae1fd1666f0
zackdesu <zarchxxx@gmail.com>
2023-09-09 18:00:43 +0700

    feat: cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -8,6 +8,24 @@ const app: Express = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use((req, res, next) => {
+  const allowedOrigins = ["http://localhost:5173"];
+  const origin = req.headers.origin;
+
+  if (origin) {
+    if (allowedOrigins.includes(origin)) {
+      res.setHeader("Access-Control-Allow-Origin", origin);
+    }
+  }
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  return next();
+});
+
 app.get("env") !== "development" ? app.set("trust proxy", 1) : null;
 
 app.get("/", (req: Request, res: Response) => {
