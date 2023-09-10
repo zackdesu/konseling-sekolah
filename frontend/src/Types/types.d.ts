34edbcac66b16fcf670a55119a369d1b39b05382
zackdesu <zarchxxx@gmail.com>
2023-09-10 08:16:54 +0700

    feat: api, user

--- frontend/src/Types/types.d.ts
+++ frontend/src/Types/types.d.ts
@@ -8,6 +8,7 @@ interface DataPost {
 }
 
 interface User {
+  id: string;
   username: string;
   realname: string;
   email: string;
@@ -15,10 +16,22 @@ interface User {
   tanggalLahir: Date;
   gender: string;
   password: string;
-  img: string;
-  likedPost: DataPost[];
-  likedQuotes: DataQuotes[];
-  mbti: string;
+  img: string | null;
+  likedPost?: DataPost[];
+  likedQuotes?: DataQuotes[];
+  mbti: string | null;
+  phonenumber: string | null;
+}
+
+interface IProfile {
+  id: string;
+  username: string;
+  realname: string;
+  tempatLahir: string;
+  tanggalLahir: Date;
+  gender: string;
+  img: string | null;
+  mbti: string | null;
 }
 
 interface DataQuotes {
@@ -26,4 +39,34 @@ interface DataQuotes {
   quote: string;
   creator: string;
   likes: number;
+}
+
+interface IRegisterData {
+  username: string;
+  realname: string;
+  email: string;
+  tempatLahir: string;
+  tanggalLahir: Date;
+  gender: string;
+  password: string;
+}
+
+interface IAPISuccess {
+  token?: string | null;
+  message: string;
+  data: IToken;
+}
+
+interface IAPIError {
+  response: { data: { message: string } };
+}
+
+interface IToken {
+  id: string;
+  username: string;
+  email: string;
+  gender: string;
+  img: string | null;
+  exp: number;
+  iat: number;
 }
