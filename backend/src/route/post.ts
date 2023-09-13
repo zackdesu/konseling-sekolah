c104640952605c39ed84f105008b43a204c2dd28
zackdesu <zarchxxx@gmail.com>
2023-09-13 20:34:58 +0700

    feat: like route

--- backend/src/route/post.ts
+++ backend/src/route/post.ts
@@ -1,5 +1,11 @@
 import { Router } from "express";
-import { createPost, deletePost, editPost, getPosts } from "../controller/post";
+import {
+  createPost,
+  deletePost,
+  editPost,
+  getPosts,
+  likePost,
+} from "../controller/post";
 import { isLoggedIn } from "../middleware/account";
 
 const router: Router = Router();
@@ -7,6 +13,7 @@ const router: Router = Router();
 router.route("/post").post(isLoggedIn, createPost).get(isLoggedIn, getPosts);
 router
   .route("/post/:id")
+  .post(isLoggedIn, likePost)
   .put(isLoggedIn, editPost)
   .delete(isLoggedIn, deletePost);
 
