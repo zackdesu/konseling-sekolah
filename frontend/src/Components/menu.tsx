2aa97e05306c7c961528aec06daccc268d94d61b
zackdesu <zarchxxx@gmail.com>
2023-09-13 13:16:24 +0700

    feat: move z-index to parent

--- frontend/src/Components/menu.tsx
+++ frontend/src/Components/menu.tsx
@@ -3,8 +3,8 @@ import { BsFillPersonFill } from "react-icons/bs";
 import { Link } from "react-router-dom";
 const Menu = () => {
   return (
-    <div className="mx-auto fixed bottom-5 left-0 right-0 max-w-[250px] w-[55%] min-[500px]:w-[40%] md:w-1/3">
-      <div className="bg-zinc-200 px-8 py-3 flex justify-between items-center rounded-full relative z-30">
+    <div className="mx-auto fixed bottom-5 left-0 right-0 max-w-[250px] w-[55%] min-[500px]:w-[40%] md:w-1/3 z-30">
+      <div className="bg-zinc-200 px-8 py-3 flex justify-between items-center rounded-full relative">
         <Link to={"/"}>
           <AiFillHome size={20} className="min-w-[20px]" />
         </Link>
