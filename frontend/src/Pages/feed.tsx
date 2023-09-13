45106c7fd34832a038a58fa4a33d4e7b14049959
zackdesu <zarchxxx@gmail.com>
2023-09-13 14:13:46 +0700

    feat: fetch feed

--- frontend/src/Pages/feed.tsx
+++ frontend/src/Pages/feed.tsx
@@ -1,21 +1,55 @@
 import { Link } from "react-router-dom";
-import { dataPost } from "../Components/data";
 import FeedPost from "../Components/feedpost";
 import Redirectuser from "../utils/redirecthome";
+import { useEffect, useState } from "react";
+import { connectApi, infoAcc, refreshAcc } from "../api/api";
 
 const Feed = () => {
   Redirectuser();
+
+  const [dataPost, setDataPost] = useState<DataPost[]>();
+  const [token, setToken] = useState<string>("");
+  const [user, setUser] = useState<IProfile>();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    refreshAcc<IAPISuccess>()
+      .then((res) => res.token && setToken(res.token))
+      .catch((err: IAPIError) => {
+        console.error(err.response.data.message);
+      });
+  }, []);
+
+  useEffect(() => {
+    if (!token) return;
+
+    infoAcc<IProfile>(token)
+      .then((res) => setUser(res))
+      .catch((err: IAPIError) => console.error(err.response.data.message));
+
+    setLoading(false);
+  }, [token]);
+
+  useEffect(() => {
+    connectApi<DataPost[]>("/post")
+      .then((res) => setDataPost(res))
+      .catch((err: IAPIError) => console.log(err.response.data.message));
+  }, []);
+
   return (
     <>
-      <div className="py-20 flex flex-wrap items-center justify-center mx-5">
+      <div className="py-20 flex flex-col flex-wrap items-center justify-center mx-5">
         <Link to={"/createfeed"} className="ml-auto normalbutton mr-12">
           + Buat Feed
         </Link>
-        {dataPost
-          .filter((e) => !e.private)
-          .map((e, i) => (
-            <FeedPost data={e} key={i} />
-          ))}
+
+        {!loading
+          ? dataPost
+            ? dataPost
+                .filter((e) => !e.private)
+                .map((e, i) => <FeedPost data={e} key={i} user={user} />)
+            : null
+          : "Loading..."}
       </div>
     </>
   );
