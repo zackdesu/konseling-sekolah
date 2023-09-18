9278feea2b146b64f5a182de8215835098973462
zackdesu <zarchxxx@gmail.com>
2023-09-18 13:28:49 +0700

    feat: quotepost page (need to change)

--- frontend/src/Components/quotepost.tsx
+++ frontend/src/Components/quotepost.tsx
@@ -2,37 +2,23 @@ import { CiMenuKebab } from "react-icons/ci";
 import { AiFillLike } from "react-icons/ai";
 import { useState } from "react";
 
-const user: User = {
-  username: "zackdesu",
-  realname: "Wongso Wijaya",
-  gender: "Laki-Laki",
-  mbti: "INFJ",
-  likedPost: [],
-  likedQuotes: [],
-  img: "/unknown.jpg",
-  email: "",
-  tempatLahir: "",
-  tanggalLahir: new Date(2007, 1, 8),
-  password: "12345678",
-};
-
 const QuotePost = ({ data }: { data: DataQuotes }) => {
-  const [liked, setLiked] = useState<boolean>();
-  const [likes, setLikes] = useState(data.likes);
+  const [liked] = useState<boolean>();
+  const [likes] = useState(data.likes);
 
-  const handleLikeClick = () => {
-    if (liked) {
-      setLiked(false);
-      setLikes(likes - 1);
-      user.likedQuotes = user.likedQuotes.filter((post) => post.id !== data.id);
-    } else {
-      setLiked(true);
-      setLikes(likes + 1);
-      console.log(user.likedQuotes);
-      user.likedQuotes.push(data);
-    }
-    console.log(user.likedQuotes);
-  };
+  // const handleLikeClick = () => {
+  //   if (liked) {
+  //     setLiked(false);
+  //     setLikes(likes - 1);
+  //     user.likedQuotes = user.likedQuotes.filter((post) => post.id !== data.id);
+  //   } else {
+  //     setLiked(true);
+  //     setLikes(likes + 1);
+  //     console.log(user.likedQuotes);
+  //     user.likedQuotes.push(data);
+  //   }
+  //   console.log(user.likedQuotes);
+  // };
 
   return (
     <div className="bg-zinc-100 rounded-lg w-[90%] max-sm:mx-auto m-2 p-2 flex flex-col justify-between">
@@ -42,7 +28,10 @@ const QuotePost = ({ data }: { data: DataQuotes }) => {
           <span className="font-medium max-sm:text-xs truncate w-[200px]">
             {data.creator}
           </span>
-          <span className="ml-10" onClick={handleLikeClick}>
+          {/* <span className="ml-10" onClick={handleLikeClick}>
+            <AiFillLike className={liked ? "text-blue-500" : ""} />
+          </span> */}
+          <span className="ml-10">
             <AiFillLike className={liked ? "text-blue-500" : ""} />
           </span>
           <p className="ml-4 text-sm">{likes}</p>
