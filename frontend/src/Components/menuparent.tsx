152a18396dbdc8a891f0a915fc810184e31b4be7
zackdesu <zarchxxx@gmail.com>
2023-09-18 13:28:15 +0700

    feat: responsive fix

--- frontend/src/Components/menuparent.tsx
+++ frontend/src/Components/menuparent.tsx
@@ -12,7 +12,7 @@ const MenuParent = ({
   children: ReactNode;
 }) => {
   return (
-    <div className="w-full min-[1092px]:h-full">
+    <div className="w-full">
       <div className="max-md:text-center md:mx-20 lg:mx-36" id={id}>
         <h3>{title}</h3>
         <p className="text-zinc-500">{desc}</p>
