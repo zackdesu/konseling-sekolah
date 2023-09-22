0c9e38a389e3edbafca134bb40a88cc1d7fbab46
zackdesu <zarchxxx@gmail.com>
2023-09-23 06:53:37 +0700

    feat: settingsmenu update href

--- frontend/src/Pages/settings.tsx
+++ frontend/src/Pages/settings.tsx
@@ -1,7 +1,7 @@
 import { IconType } from "react-icons";
 import { BsPersonFill } from "react-icons/bs";
 import { connectApi } from "../api/api";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Redirectuser from "../utils/redirecthome";
 import toast from "react-hot-toast";
 
@@ -12,16 +12,21 @@ const Settings = () => {
     icon: Icon,
     title,
     desc,
+    href,
   }: {
     icon: IconType;
     title: string;
     desc: string;
+    href: string;
   }) => (
-    <div className="bg-zinc-100 p-2 border-2 w-[95%] sm:w-8/12 md:w-1/2 h-[60px] grid grid-rows-2 grid-cols-[60px_minmax(100px,_1fr)] auto-cols-[1fr,2fr,2fr] cursor-pointer mb-5">
+    <Link
+      to={href}
+      className="bg-zinc-100 p-2 border-2 w-[95%] sm:w-8/12 md:w-1/2 h-[60px] grid grid-rows-2 grid-cols-[60px_minmax(100px,_1fr)] auto-cols-[1fr,2fr,2fr] cursor-pointer mb-5"
+    >
       <Icon className="row-span-2 w-[35px] h-[35px] place-self-center rounded-full p-1 border" />
       <h5 className="self-end">{title}</h5>
       <p className="text-zinc-500">{desc}</p>
-    </div>
+    </Link>
   );
 
   const navigate = useNavigate();
@@ -42,6 +47,7 @@ const Settings = () => {
         icon={BsPersonFill as IconType}
         title="Informasi Akun"
         desc="Rincian akun anda"
+        href="/settings/options"
       />
       <button className="normalbutton bg-red-600" onClick={handleLogOut}>
         Log Out
