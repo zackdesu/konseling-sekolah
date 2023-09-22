980c3f830ea9b3bde7540f2574ccb0e2d91aaab6
zackdesu <zarchxxx@gmail.com>
2023-09-23 06:18:51 +0700

    feat: limit update to 5 & message update to json

--- backend/src/middleware/rateLimit.ts
+++ backend/src/middleware/rateLimit.ts
@@ -4,8 +4,12 @@ import { rateLimit } from "express-rate-limit";
 import { Request, Response } from "express";
 export const registerLimit = rateLimit({
   windowMs: 1000 * 60 * 60 * 12,
-  limit: 1,
-  message: "Kamu bisa membuat akun lagi setelah 12 jam.",
+  limit: 5,
+  message: (req: Request, res: Response) =>
+    res.json({
+      message:
+        "Kamu memasukkan banyak password yang salah. Tunggu setelah 12 jam.",
+    }),
 });
 
 export const loginLimit = rateLimit({
