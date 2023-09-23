5de2ea07a814e926ac700ff1740092d7f752ea0f
zackdesu <zarchxxx@gmail.com>
2023-09-23 14:33:32 +0700

    feat: simplified import

---
+++ frontend/src/Pages/index.ts
@@ -0,0 +1,36 @@
+import Home from "./home";
+import Login from "./login";
+import Register from "./register";
+import Profile from "./profile";
+import LandingPage from "./landingpage";
+import NotFound from "./notfound";
+import Tentang from "./tentang";
+import Settings from "./settings";
+import Feed from "./feed";
+import Talks from "./talks";
+import Quiz from "./quiz";
+import CreateFeed from "./createfeed";
+import Quotes from "./quotes";
+import EditAcc from "./editacc";
+import Options from "./options";
+import Dashboard from "./dashboard";
+import Counselor from "./counselor";
+export {
+  Home,
+  Login,
+  Register,
+  Profile,
+  LandingPage,
+  NotFound,
+  Tentang,
+  Settings,
+  Feed,
+  Talks,
+  Quiz,
+  CreateFeed,
+  Quotes,
+  EditAcc,
+  Options,
+  Dashboard,
+  Counselor,
+};
