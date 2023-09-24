b98d5f5d3d076186b09c721adf189cb1330ce1c6
zackdesu <zarchxxx@gmail.com>
2023-09-24 09:49:29 +0700

    feat: using index name

--- backend/src/route/index.ts
+++ backend/src/route/index.ts
@@ -1,5 +1,6 @@
 import { router as account } from "./account";
 import { router as post } from "./post";
 import { router as admin } from "./admin";
+import { router as counselor } from "./counselor";
 
-export { account, post, admin };
+export { account, post, admin, counselor };
