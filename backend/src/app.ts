43e6097298a2d20121175d76a284574efbfb6e79
zackdesu <zarchxxx@gmail.com>
2023-10-01 03:34:42 +0700

    feat: allowed url

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use((req, res, next) => {
-  const allowedOrigins = ["http://localhost:5173"];
+  const allowedOrigins = ["http://localhost:5173", process.env.ALLOWED_URL!];
   const origin = req.headers.origin;
 
   if (origin) {
